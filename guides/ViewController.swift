65a5ba84b1be77a32241d21b7b41fb219024e73b
kuquert <marcus.kuquert@icloud.com>
2019-08-21 17:19:59 -0300

    Add tableView and populate it with response

--- guides/ViewController.swift
+++ guides/ViewController.swift
@@ -10,8 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var guides: [Guide]?
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    private func tableViewSetup() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableViewSetup()
         loadGuides()
     }
 
@@ -20,7 +30,7 @@ class ViewController: UIViewController {
         var urlRequest = URLRequest(url: url)
         urlRequest.httpMethod = "GET"
         
-        URLSession.shared.dataTask(with: urlRequest) { (data: Data?, response: URLResponse?, error: Error?) in
+        URLSession.shared.dataTask(with: urlRequest) { [weak self] (data: Data?, response: URLResponse?, error: Error?) in
             guard error == nil else {
                 print(error!)
                 return
@@ -32,9 +42,36 @@ class ViewController: UIViewController {
             }
             
             let decoder = JSONDecoder()
-            let guides = try! decoder.decode(GuidesResponse.self, from: data)
+            let guidesResponse = try! decoder.decode(GuidesResponse.self, from: data)
+            let guides = guidesResponse.data
             print(guides)
+            DispatchQueue.main.async {
+                self?.guides = guides
+                self?.tableView.reloadData()
+            }
         }.resume()
     }
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return guides?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let guide = guides?[indexPath.row] else {
+            return UITableViewCell()
+        }
+        
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: "guideTableViewCell")
+        cell.textLabel?.text = guide.name
+        cell.detailTextLabel?.text = guide.url
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
