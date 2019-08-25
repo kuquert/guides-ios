de82c62a2472765cf3900a121aee706da27b98b5
kuquert <marcus.kuquert@icloud.com>
2019-08-24 22:23:16 -0300

    Remove unneeded GuideView class and improve cell

--- guides/Controller/ViewController.swift
+++ guides/Controller/ViewController.swift
@@ -9,15 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var tableView: UITableView!
-
-    private var groupedGuides: [String : [Guide]] = [:]
+    @IBOutlet var tableView: UITableView!
+    
+    private let cellId = "GuideTableViewCell"
+    private var groupedGuides: [String: [Guide]] = [:]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableViewSetup()
-        GuideFacade.shared.loadUpcomingGuides { [weak self] (upcomingGuides, error) in
+        setupTableView()
+        loadUpcommingGuides()
+    }
+
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(GuideTableViewCell.self, forCellReuseIdentifier: cellId)
+    }
+
+    private func loadUpcommingGuides() {
+        GuideFacade.shared.loadUpcomingGuides { [weak self] upcomingGuides, _ in
             guard let guides = upcomingGuides?.data else {
                 return
             }
@@ -25,14 +35,9 @@ class ViewController: UIViewController {
             self?.tableView.reloadData()
         }
     }
-    
-    private func tableViewSetup() {
-        tableView.delegate = self
-        tableView.dataSource = self
-    }
-    
-    private static func groupByDate(guides: [Guide]) -> [String : [Guide]] {
-        var dict: [String : [Guide]] = [:]
+
+    private static func groupByDate(guides: [Guide]) -> [String: [Guide]] {
+        var dict: [String: [Guide]] = [:]
         guides.forEach {
             var temp = dict.removeValue(forKey: $0.startDate) ?? []
             temp.append($0)
@@ -43,33 +48,33 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: UITableViewDataSource {
-    func numberOfSections(in tableView: UITableView) -> Int {
+    func numberOfSections(in _: UITableView) -> Int {
         return groupedGuides.keys.count
     }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return groupedGuides.keys.sorted()[section]
+
+    func tableView(_: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Starting on " + groupedGuides.keys.sorted()[section]
     }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+    func tableView(_: UITableView, numberOfRowsInSection section: Int) -> Int {
         let keyForSection = groupedGuides.keys.sorted()[section]
         return groupedGuides[keyForSection]?.count ?? 0
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let keyForSection = groupedGuides.keys.sorted()[indexPath.section]
-        let guides_ = groupedGuides[keyForSection]
-        
-        guard let guide = guides_?[indexPath.row] else {
+        let guides = groupedGuides[keyForSection]
+
+        guard let guide = guides?[indexPath.row] else {
             return UITableViewCell()
         }
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "GuideTableViewCell",
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId,
                                                  for: indexPath) as! GuideTableViewCell
-        cell.render(guide: guide)
+        cell.guide = guide
         return cell
     }
-    
+
     func tableView(_: UITableView, heightForRowAt _: IndexPath) -> CGFloat {
         return UITableView.automaticDimension
     }
--- guides/View/GuideTableViewCell.swift
+++ guides/View/GuideTableViewCell.swift
@@ -9,15 +9,47 @@
 import UIKit
 
 @IBDesignable
-class GuideTableViewCell: UITableViewCell {
-    
-    @IBOutlet var guideView: GuideView!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
+class GuideTableViewCell: UITableViewCell, NibLoadable {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLable: UILabel!
+    @IBOutlet weak var locationLable: UILabel!
+
+    var guide: Guide? {
+        didSet {
+            titleLabel.text = guide?.name
+            detailLable.text = endDateFormatter(guide)
+            locationLable.text = venueFormatter(guide)
+        }
     }
-    
-    func render(guide: Guide) {
-        guideView.guide = guide
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        fromNib()
+    }
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        fromNib()
+    }
+
+    private func venueFormatter(_ guide: Guide?) -> String {
+        if let city = guide?.venue.city, let state = guide?.venue.state {
+            return "\(city), \(state)"
+        } else if let city = guide?.venue.city {
+            return "\(city)"
+        } else if let state = guide?.venue.state {
+            return "\(state)"
+        } else {
+            return  ""
+        }
+    }
+
+    private func endDateFormatter(_ guide: Guide?) -> String {
+        if let endDate = guide?.endDate {
+            return "Ending on \(endDate)"
+        } else {
+            return ""
+        }
     }
 }
